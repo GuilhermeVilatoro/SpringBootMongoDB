7089b5828160374a4383e76c18a9d0409a39a239
GuilhermeVilatoro <vilatorog@gmail.com>
2020-10-31 01:53:58 -0300

    commit parcial 2.

--- src/main/java/com/springboot/mongo/services/PostService.java
+++ src/main/java/com/springboot/mongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.springboot.mongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,16 @@ public class PostService {
 	@Autowired
 	private IPostRepository postRepository;
 
+	public List<Post> findByTitle(String title) {
+		return postRepository.findByTitleContainingIgnoreCase(title);
+	}
+
+	public List<Post> searchTitle(String title) {
+		return postRepository.searchTitle(title);
+	}
+
 	public Post findById(String id) {
 		Optional<Post> post = postRepository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-	}	
+	}
 }
